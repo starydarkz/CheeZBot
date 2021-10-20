04b414f32111de2c5eaaa0f46e619350f17cb2e3
Stary <76498661+starydarkz@users.noreply.github.com>
2021-10-20 11:35:23 -0400

    Version 1, lanzamiento

--- cheebot.py
+++ cheebot.py
@@ -1,5 +1,9 @@
-#version:0.2 by StaryDark
+'''
+Version: 1.0
 
+Bot de la Comunidad Hacking Etico en Español > t.me/HackingEticoEs
+Alias del bot: t.me/thechee_bot
+'''
 from telegram.ext import Updater, CommandHandler, Filters
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ChatAction
 import functions
@@ -11,8 +15,10 @@ print ("\nCheeZBot ha sido iniciado...")
 updater = Updater(token=token, use_context=True)
 admins = ["@Dark_zly"]
 
-#Commands
+#Commandos generales
 def start(update, context):
+    ''' Saluda al bot'''
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
     context.bot.send_message(chat_id=update.effective_chat.id, text="Estoy despierto")
 
 def quees(update, context):
@@ -21,6 +27,7 @@ def quees(update, context):
     answer = functions.definiciones(user_say)
     update.message.reply_text(answer)    
 
+#Comandos de administracion
 def addquestions(update, context):
     user_say = " ".join(context.args)
     functions.add_questions(user_say)
@@ -32,32 +39,36 @@ def readquestions(update, context):
 
 isalive = [False, False] #primer= Parar proceso segundo = Evitar mas de un subproceso
 def whiledayq(context, update, isalive):
+    '''Complemento de preguntas random'''
     isalive[1] = True
     while isalive[0]:
         questions = functions.random_questions()
         context.bot.send_message(chat_id=update.effective_chat.id, text=questions)
-        time.sleep(720)
+        time.sleep(43200)
 
 def randomquestions(update, context, isalive=isalive):
+    '''Activa las preguntas random'''
     
     whiledayquestions = threading.Thread(target=whiledayq, args=(context, update, isalive))
     if isalive[1]:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Pregunta del dia ya esta ON")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Pregunta del dia esta: ON")
     else:
         isalive[0] = True
         whiledayquestions.start()
 
 def offdayquestion(update, context, isalive=isalive):
+    '''Desactiva la funcion de preguntas random'''
+    
     context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
-
     isalive[0] = False
     isalive[1] = False
     context.bot.send_message(chat_id=update.effective_chat.id, text="Pregunta del dia desactivado")
 
-#Listeners 
+#Comandos publicos
 start_handler = CommandHandler('start', start,)
 quees_handler = CommandHandler('quees', quees, pass_args=True)
 
+#Comandos de administracion
 addq_handler = CommandHandler('addq', addquestions, Filters.user(username=admins), pass_args=True)
 readq_handler = CommandHandler('readq', readquestions, Filters.user(username=admins))
 random_handler = CommandHandler('randomq', randomquestions, Filters.user(username=admins))
--- functions.py
+++ functions.py
@@ -41,24 +41,19 @@ def config():
 
 def definiciones(palabra):
     """Busquedas de definiciones en wikipedia """
-    
+    #Config library wikipedia
     wikipedia.set_lang("es")
-    try:
+    
+    try: #Obtener la informacion y retornarla
         resumen = wikipedia.summary(palabra, sentences = 1)
         url = (wikipedia.page(palabra).url)
         resultado = (resumen + "\n\nMas info: " + url)
         return resultado
-    except:
-        #search = wikipedia.search(palabra, results=3)
+    except: #Si no encuentra la info mensaje de error
         error =  ("No se ha encontrado esta definicion...")
-        """resultado = ""
-        for num in range(len(search)):
-            resultado = (resultado + f"{num}-> " + search[num] + "\n")
-        resultado = (error + resultado)
-        return resultado"""
         return error
 
-#Pregunta del dia
+#Preguntas aleatorias
 def add_questions(user_say):
     """Añadir preguntas a la base de datos """
     
@@ -80,3 +75,6 @@ def random_questions():
     question = random.choice(open("db/questions.txt").readlines())
     question = ("🛎La pregunta del dia es:🛎\n\n" + question)
     return question
+
+def help():
+    pass
